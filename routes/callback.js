715f07bcc3ad45241172f174470a75c1622c8826
Guilherme Elias <guilherme.elias@gmail.com>
2019-01-22 11:25:21 -0200

    Adding callback code

--- routes/callback.js
+++ routes/callback.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('callback', { title: 'Successfully callbacked' });
+  var code = req.query.code
+  res.render('callback', { title: 'Successfully callbacked', code: code });
+  
 });
 
 module.exports = router;
