fd853c19813aa6ed32212d5c4d9af54e4e3c6d78
Guilherme Elias <guilherme.elias@gmail.com>
2019-01-23 17:36:38 -0200

    ok

--- routes/callback.js
+++ routes/callback.js
@@ -1,18 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var requestify = require('requestify');
 
 router.get('/', function(req, res, next) {
   var code = req.query.code
   var state = req.query.state
-  res.render('callback', { title: 'Successfully callbacked', code: code, state: state });
-  
-});
 
-router.get('/authorized', function(req, res, next) {
-  var access_token = req.query.access_token
-  var token_type = req.query.token_type
-  res.render('authorized', { title: 'Successfully Authorized', access_token: access_token, token_type: token_type });
+  requestify.request('https://www.googleapis.com/oauth2/v4/token', {
+      method: 'POST',
+      dataType: 'form-url-encoded',
+      body: {
+        client_id: '907113127809-0pmnffpdsuf6epqbs7g41215pke1eum7.apps.googleusercontent.com'
+        ,client_secret: 'S2zu9HwDrdw4jKZZR65kcs8a'
+        ,code: code
+        ,redirect_uri: 'https://eliasopenidconnect.herokuapp.com/callback'
+        ,grant_type: 'authorization_code' 
+      }
+    }).then((response) => {
+      console.log(response)
+      var res_json = JSON.parse(response.body)
+      var access_token = res_json.access_token
+      var id_token = res_json.id_token
+      var schema_url = 'umov://openid?access_token='+access_token+'&id_token='+id_token
+      console.log(schema_url)
+      res.render('callback', { title: 'Successfully callbacked', schema_url: 'http://google.com'});
+    }).fail(function(response) {
+      console.log(response)
+      res.render('callback', { title: 'ERROR callbacked', schema_url: 'http://pudim.com.br' });
+    });
+
+  //res.render('callback', { title: 'empty callbacked', schema_url: 'http://google.com' });
   
 });
 
 module.exports = router;
+
+
